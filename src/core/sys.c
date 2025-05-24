1d9f3742d265c337d0857e131864b5e11606fc54
Christopher Gilliard <christopher.gilliard@gmail.com>
2025-05-23 17:27:19 -0700

    set error code for mmap

--- src/core/sys.c
+++ src/core/sys.c
@@ -171,9 +171,13 @@ int lseek(int fd, off_t offset, int whence) {
 	IMPL_WRAPPER(int, lseek, fd, offset, whence)
 }
 
+// to get the error code
+#include <errno.h>
+
 void *mmap(void *addr, size_t length, int prot, int flags, int fd,
 	   off_t offset) {
 	void *v = syscall_mmap(addr, length, prot, flags, fd, offset);
+	err = errno;
 	return v;
 }
 
--- src/store/btree.c
+++ src/store/btree.c
@@ -1,3 +1,3 @@
-// #include <btree.h>
+#include <btree.h>
 
 int x123(void) { return 0; }
