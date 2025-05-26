f07abfa4003b2bda1a737397a8b9b70b300d2ef0
Christopher Gilliard <christopher.gilliard@gmail.com>
2025-05-26 00:50:34 -0700

    linux impl

--- src/core/sys.c
+++ src/core/sys.c
@@ -30,6 +30,116 @@
 #include <types.h>
 
 #ifdef __linux__
+#define DECLARE_SYSCALL(ret_type, name, num, ...) \
+	ret_type syscall_##name(__VA_ARGS__);     \
+	__asm__(".global syscall_" #name          \
+		"\n"                              \
+		"syscall_" #name                  \
+		":\n"                             \
+		"    movq $" #num                 \
+		", %rax\n"                        \
+		"    syscall\n"                   \
+		"    ret\n");
+
+void *syscall_mmap(void *addr, size_t length, int prot, int flags, int fd,
+		   off_t offset) {
+	void *result;
+	__asm__ volatile(
+	    "movq %1, %%rdi\n"
+	    "movq %2, %%rsi\n"
+	    "movq %3, %%rdx\n"
+	    "movq %4, %%r10\n"
+	    "movq %5, %%r8\n"
+	    "movq %6, %%r9\n"
+	    "movq $9, %%rax\n"
+	    "syscall\n"
+	    "movq %%rax, %0\n"
+	    : "=r"(result)
+	    : "r"((uint64_t)addr), "r"(length), "r"((uint64_t)prot),
+	      "r"((uint64_t)flags), "r"((uint64_t)fd), "r"((uint64_t)offset)
+	    : "%rax", "%rdi", "%rsi", "%rdx", "%r10", "%r8", "%r9");
+	return result;
+}
+
+#define IMPL_WRAPPER(ret_type, name, ...)         \
+	ret_type v = syscall_##name(__VA_ARGS__); \
+	if (v < 0) {                              \
+		err = -v;                         \
+		return -1;                        \
+	}                                         \
+	return v;
+
+DECLARE_SYSCALL(int, sched_yield, 24, 158, void)
+
+DECLARE_SYSCALL(ssize_t, write, 1, 4, int fd, const void *buf, size_t length)
+
+DECLARE_SYSCALL(void, exit, 60, 1, int code)
+
+DECLARE_SYSCALL(int, munmap, 11, 73, void *addr, size_t length)
+
+DECLARE_SYSCALL_OPEN(int, open, 2, 5, const char *pathname, int flags, ...)
+
+DECLARE_SYSCALL(int, close, 3, 6, int fd)
+
+DECLARE_SYSCALL(int, ftruncate, 77, 201, int fd, long length)
+
+DECLARE_SYSCALL(int, msync, 26, 65, void *addr, unsigned long length, int flags)
+
+DECLARE_SYSCALL(off_t, lseek, 8, 199, int fd, off_t offset, int whence)
+
+DECLARE_SYSCALL(int, fdatasync, 187, 75, int fd)
+
+DECLARE_SYSCALL(int, fork, 57, 2, void)
+
+DECLARE_SYSCALL(int, pipe, 22, 42, int fd[2])
+
+DECLARE_SYSCALL(int, nanosleep, 35, 35, const struct timespec *duration,
+		struct timespec *rem)
+
+DECLARE_SYSCALL(int, gettimeofday, 96, 96, struct timeval *tv, void *tz)
+
+DECLARE_SYSCALL(int, settimeofday, 170, 170, const struct timeval *tv,
+		const void *tz)
+
+int sys_sched_yield(void) {
+	int v = syscall_sched_yield();
+	if (v < 0) {
+		err = -v;
+		return -1;
+	}
+	return v;
+}
+
+ssize_t sys_write(int fd, const void *buf, size_t length) {
+	IMPL_WRAPPER(ssize_t, write, fd, buf, length)
+}
+
+void sys_exit(int code) {
+	syscall_exit(code);
+	while (true);
+}
+
+int sys_open(const char *pathname, int flags, ...) {
+	IMPL_WRAPPER(int, open, pathname, flags, 0600)
+}
+
+int sys_close(int fd) { IMPL_WRAPPER(int, close, fd) }
+int sys_ftruncate(int fd, off_t length) {
+	IMPL_WRAPPER(int, ftruncate, fd, length)
+}
+int sys_msync(void *addr, size_t length,
+	      int flags){IMPL_WRAPPER(int, msync, addr, length, flags)}
+
+off_t sys_lseek(int fd, off_t offset, int whence) {
+	IMPL_WRAPPER(int, lseek, fd, offset, whence)
+}
+
+int sys_munmap(void *addr, size_t length) {
+	IMPL_WRAPPER(int, munmap, addr, length)
+}
+
+int sys_fdatasync(int fd){IMPL_WRAPPER(int, fdatasync, fd)}
+
 #elif defined(__APPLE__)
 
 ssize_t write(int fd, const void *buf, size_t length);
