16ee5870387ec6a84ce6bdc19f957adf91e8cd4f
Christopher Gilliard <christopher.gilliard@gmail.com>
2025-05-26 16:53:53 -0700

    alloc test

--- src/core/test.c
+++ src/core/test.c
@@ -1,3 +1,4 @@
+#include <alloc.h>
 #include <criterion/criterion.h>
 #include <error.h>
 #include <lock.h>
@@ -185,4 +186,46 @@ Test(core, lock) {
 		cr_assert_eq(l1, (0x1UL << 63));
 	}
 	cr_assert_eq(l1, 0);
+}
+
+Test(alloc, alloc1) {
+	size_t last_address;
+	char *test1 = alloc(1000 * 1000 * 5);
+	release(test1);
+
+	void *a = alloc(10);
+
+	last_address = 0;
+	for (int i = 0; i < 32000; i++) {
+		char *test2 = alloc(10);
+		// after first loop, we should just be reusing addresses
+		if (last_address)
+			cr_assert_eq((size_t)test2, last_address);
+		else
+			cr_assert(a != test2);
+		cr_assert(test2 != NULL);
+		test2[0] = 'a';
+		test2[1] = 'b';
+		release(test2);
+		last_address = (size_t)test2;
+	}
+
+	release(a);
+
+	// use a different size
+	int size = 1000;
+	char *values[size];
+	last_address = 0;
+	for (int i = 0; i < size; i++) {
+		values[i] = alloc(32);
+		// after first loop, we should be incrementing by 32 with each
+		// address
+		if (last_address)
+			cr_assert_eq((size_t)values[i], last_address + 32);
+		last_address = (size_t)values[i];
+	}
+
+	for (int i = 0; i < size; i++) {
+		release(values[i]);
+	}
 }
