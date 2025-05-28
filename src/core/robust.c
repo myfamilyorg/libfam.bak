2eea55b03899ef94031b5c5e8070ee5e096367c0
Christopher Gilliard <christopher.gilliard@gmail.com>
2025-05-28 13:37:23 -0700

    improve robust error handling and other things

--- src/core/robust.c
+++ src/core/robust.c
@@ -17,49 +17,54 @@ STATIC void init_robust_ctx(RobustCtx *ctx) {
 	while (ctx->port == 0) {
 		address.sin_port = 0;
 		ctx->sock = socket(AF_INET, SOCK_STREAM, 0);
-		if (ctx->sock == -1) continue;
+		if (ctx->sock == -1) {
+			print_error("socket");
+			continue;
+		}
 		if (setsockopt(ctx->sock, SOL_SOCKET, SO_REUSEADDR, &OPT,
 			       sizeof(OPT)) == -1) {
+			print_error("setsocketop");
 			close(ctx->sock);
 			continue;
 		}
 		if (bind(ctx->sock, (struct sockaddr *)&address,
 			 sizeof(address)) == -1) {
+			print_error("bind");
 			close(ctx->sock);
 			continue;
 		}
 		addr_len = sizeof(address);
 		if (getsockname(ctx->sock, (struct sockaddr *)&address,
 				&addr_len) == -1) {
+			print_error("getsockname");
 			close(ctx->sock);
 			continue;
 		}
 		ctx->port = ntohs(address.sin_port);
-		if (ctx->port <= 0) close(ctx->sock);
+		if (ctx->port == 0) {
+			print_error("port==0");
+			close(ctx->sock);
+		}
 	}
 }
 RobustGuard robust_lock(RobustCtx *ctx, RobustLock *lock) {
-	uint16_t desired = 0, expected = 0;
+	uint16_t desired = 0, expected = 0, port;
 	struct sockaddr_in address;
 	int sock;
 	uint64_t counter = 0;
 
 	if (ctx->port == 0) init_robust_ctx(ctx);
+	memcpy(&address.sin_addr.s_addr, ADDR, 4);
+	address.sin_family = AF_INET;
+	desired = ctx->port;
 
 start_loop:
 	do {
 		if (counter++) yield();
-		uint16_t cur = __atomic_load_n(lock, __ATOMIC_ACQUIRE);
-		if (cur == 0) {
+		if ((port = __atomic_load_n(lock, __ATOMIC_ACQUIRE)) == 0) {
 			expected = 0;
-			desired = ctx->port;
 		} else {
-			uint16_t port = cur;
-
-			address.sin_family = AF_INET;
 			address.sin_port = htons(port);
-			memcpy(&address.sin_addr.s_addr, ADDR, 4);
-
 			sock = socket(AF_INET, SOCK_STREAM, 0);
 			if (sock == -1) goto start_loop;
 			if (setsockopt(sock, SOL_SOCKET, SO_REUSEADDR, &OPT,
@@ -67,7 +72,6 @@ start_loop:
 				close(sock);
 				goto start_loop;
 			}
-			err = 0;
 			if (bind(sock, (struct sockaddr *)&address,
 				 sizeof(address)) == 0) {
 				close(ctx->sock);
