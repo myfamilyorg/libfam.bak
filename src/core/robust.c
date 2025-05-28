3fef6175fa1fad2bda422b76873108f0621936fb
Christopher Gilliard <christopher.gilliard@gmail.com>
2025-05-27 22:48:00 -0700

    improve error handling

--- src/core/robust.c
+++ src/core/robust.c
@@ -1,4 +1,5 @@
 #include <arpa/inet.h>
+#include <error.h>
 #include <misc.h>
 #include <robust.h>
 #include <stdio.h>
@@ -6,22 +7,37 @@
 
 #define PORT 9999
 
-STATIC int init_robust_ctx(RobustCtx *ctx) {
+STATIC void init_robust_ctx(RobustCtx *ctx) {
 	byte addr[4] = {127, 0, 0, 1};
-	struct sockaddr_in address;
 	int opt = 1;
 
-	address.sin_family = AF_INET;
-	memcpy(&address.sin_addr.s_addr, addr, 4);
+	while (ctx->port == 0) {
+		struct sockaddr_in address;
+		address.sin_family = AF_INET;
+		address.sin_port = 0;
+		memcpy(&address.sin_addr.s_addr, addr, 4);
 
-	ctx->sock = socket(AF_INET, SOCK_STREAM, 0);
-	setsockopt(ctx->sock, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
-	bind(ctx->sock, (struct sockaddr *)&address, sizeof(address));
-	unsigned int addr_len = sizeof(address);
-	getsockname(ctx->sock, (struct sockaddr *)&address, &addr_len);
-	ctx->port = ntohs(address.sin_port);
-
-	return 0;
+		ctx->sock = socket(AF_INET, SOCK_STREAM, 0);
+		if (ctx->sock == -1) continue;
+		if (setsockopt(ctx->sock, SOL_SOCKET, SO_REUSEADDR, &opt,
+			       sizeof(opt)) == -1) {
+			close(ctx->sock);
+			continue;
+		}
+		if (bind(ctx->sock, (struct sockaddr *)&address,
+			 sizeof(address)) == -1) {
+			close(ctx->sock);
+			continue;
+		}
+		unsigned int addr_len = sizeof(address);
+		if (getsockname(ctx->sock, (struct sockaddr *)&address,
+				&addr_len) == -1) {
+			close(ctx->sock);
+			continue;
+		}
+		ctx->port = ntohs(address.sin_port);
+		if (ctx->port <= 0) close(ctx->sock);
+	}
 }
 
 RobustGuard robust_lock(RobustCtx *ctx, RobustLock *lock) {
@@ -31,17 +47,14 @@ RobustGuard robust_lock(RobustCtx *ctx, RobustLock *lock) {
 	int opt = 1;
 	int sock;
 
-	if (ctx->sock == 0) init_robust_ctx(ctx);
+	if (ctx->port == 0) init_robust_ctx(ctx);
 
 	address.sin_family = AF_INET;
 	memcpy(&address.sin_addr.s_addr, addr, 4);
 
-	bool is_first = true;
+	uint64_t counter = 0;
 	do {
-		if (!is_first) {
-			yield();
-		}
-		is_first = false;
+		if (counter++) yield();
 		uint64_t cur = __atomic_load_n(lock, __ATOMIC_ACQUIRE);
 		if (cur == 0) {
 			expected = 0;
@@ -49,8 +62,12 @@ RobustGuard robust_lock(RobustCtx *ctx, RobustLock *lock) {
 		} else {
 			uint16_t port = cur >> 48;
 			sock = socket(AF_INET, SOCK_STREAM, 0);
-			setsockopt(sock, SOL_SOCKET, SO_REUSEADDR, &opt,
-				   sizeof(opt));
+			if (sock == -1) continue;
+			if (setsockopt(sock, SOL_SOCKET, SO_REUSEADDR, &opt,
+				       sizeof(opt)) == -1) {
+				close(sock);
+				continue;
+			}
 			if (bind(sock, (struct sockaddr *)&address,
 				 sizeof(address)) == 0) {
 				close(ctx->sock);
@@ -58,6 +75,7 @@ RobustGuard robust_lock(RobustCtx *ctx, RobustLock *lock) {
 				ctx->port = port;
 				break;
 			} else {
+				close(sock);
 				continue;
 			}
 		}
@@ -75,4 +93,6 @@ int robust_unlock(RobustLock *lock) {
 
 int robust_ctx_cleanup(RobustCtx *ctx) { return close(ctx->sock); }
 
-void robustguard_cleanup(RobustGuardImpl *rg) { robust_unlock(rg->lock); }
+void robustguard_cleanup(RobustGuardImpl *rg) {
+	if (rg->lock) robust_unlock(rg->lock);
+}
