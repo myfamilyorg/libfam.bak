0115b904c9d449764d3ce0b40786e8073d04272e
Christopher Gilliard <christopher.gilliard@gmail.com>
2025-05-30 15:20:01 -0700

    include lock

--- src/core/lock.c
+++ src/core/lock.c
@@ -1,4 +1,75 @@
-STATIC int x(void) { return 0; }
+/********************************************************************************
+ * MIT License
+ *
+ * Copyright (c) 2025 Christopher Gilliard
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ *******************************************************************************/
 
-int y(void) { return x(); }
+#include <atomic.h>
+#include <lock.h>
+#include <sys.h>
 
+#define WFLAG (0x1UL << 63)
+#define WREQUEST (0x1UL << 62)
+
+void lockguard_cleanup(LockGuardImpl *lg) {
+	if (!lg->lock) return;
+	if (lg->is_write)
+		ASTORE(lg->lock, 0);
+	else
+		ASUB(lg->lock, 1);
+}
+
+LockGuardImpl lock_read(Lock *lock) {
+	uint64_t state, desired, do_yield = 0;
+	LockGuardImpl ret;
+	do {
+		if (do_yield++) yield();
+		state = ALOAD(lock) & ~(WFLAG | WREQUEST);
+		desired = state + 1;
+	} while (!CAS(lock, &state, desired));
+	ret.lock = lock;
+	ret.is_write = 0;
+	return ret;
+}
+
+LockGuardImpl lock_write(Lock *lock) {
+	uint64_t state, desired, do_yield = 0;
+	LockGuardImpl ret;
+	do {
+		if (do_yield++) yield();
+		state = ALOAD(lock) & ~(WFLAG | WREQUEST);
+		desired = state | WREQUEST;
+	} while (!CAS(lock, &state, desired));
+
+	desired = WFLAG;
+start_loop:
+	do {
+		state = ALOAD(lock);
+		if (state != WREQUEST) {
+			yield();
+			goto start_loop;
+		}
+	} while (!CAS(lock, &state, desired));
+	ret.lock = lock;
+	ret.is_write = 1;
+	return ret;
+}
