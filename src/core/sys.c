3d436ede8291021d0bdf520fcf8f394794fbb0c8
Christopher Gilliard <christopher.gilliard@gmail.com>
2025-05-31 16:17:33 -0700

    sys.c

--- src/core/sys.c
+++ src/core/sys.c
@@ -32,8 +32,11 @@
 
 #include <error.h>
 #include <fcntl.h>
+#include <signal.h>
 #include <sys.h>
 #include <sys/mman.h>
+#include <sys/time.h>
+#include <time.h>
 #include <unistd.h>
 
 #ifdef __linux__
@@ -45,7 +48,6 @@ int sched_yield(void);
 int fdatasync(int);
 #include <errno.h>
 #include <sys/event.h>
-#include <sys/time.h>
 #include <unistd.h>
 STATIC_ASSERT(sizeof(Event) == sizeof(struct kevent), event_match);
 #else
@@ -408,84 +410,91 @@ int gettimeofday(struct timeval *tv, void *tz) {
 	int ret = syscall_gettimeofday(tv, tz);
 	SET_ERR
 }
-int settimeofday(const struct timeval *tv, const void *tz) {
-	int ret = syscall_settimeofday(tv, tz);
-	SET_ERR
-}
-int epoll_create1(int flags) {
-	int ret = syscall_epoll_create1(flags);
-	SET_ERR
-}
-int epoll_wait(int epfd, struct epoll_event *events, int maxevents,
-	       int timeout) {
-	int ret = syscall_epoll_wait(epfd, events, maxevents, timeout);
-	SET_ERR
-}
-int epoll_ctl(int epfd, int op, int fd, struct epoll_event *event) {
-	int ret = syscall_epoll_ctl(epfd, op, fd, event);
-	SET_ERR
-}
 
-int open(const char *pathname, int flags, ...) {
-	mode_t mode = 0;
-	int ret;
-	if (flags & 0100 /* O_CREAT */) {
-		__builtin_va_list ap;
-		__builtin_va_start(ap, flags);
-		mode = __builtin_va_arg(ap, mode_t);
-		__builtin_va_end(ap);
+#ifdef __linux__
+int settimeofday(const struct timeval *tv, const struct timezone *tz) {
+#endif
+#ifdef __APPLE__
+	int settimeofday(const struct timeval *tv, const void *tz) {
+#endif
+		int ret = syscall_settimeofday(tv, tz);
+		SET_ERR
 	}
-	ret = syscall_open(pathname, flags, mode);
-	SET_ERR
-}
+	int epoll_create1(int flags) {
+		int ret = syscall_epoll_create1(flags);
+		SET_ERR
+	}
+	int epoll_wait(int epfd, struct epoll_event *events, int maxevents,
+		       int timeout) {
+		int ret = syscall_epoll_wait(epfd, events, maxevents, timeout);
+		SET_ERR
+	}
+	int epoll_ctl(int epfd, int op, int fd, struct epoll_event *event) {
+		int ret = syscall_epoll_ctl(epfd, op, fd, event);
+		SET_ERR
+	}
 
-off_t lseek(int fd, off_t offset, int whence) {
-	off_t ret = syscall_lseek(fd, offset, whence);
-	SET_ERR
-}
+	int open(const char *pathname, int flags, ...) {
+		mode_t mode = 0;
+		int ret;
+		if (flags & 0100 /* O_CREAT */) {
+			__builtin_va_list ap;
+			__builtin_va_start(ap, flags);
+			mode = __builtin_va_arg(ap, mode_t);
+			__builtin_va_end(ap);
+		}
+		ret = syscall_open(pathname, flags, mode);
+		SET_ERR
+	}
+
+	off_t lseek(int fd, off_t offset, int whence) {
+		off_t ret = syscall_lseek(fd, offset, whence);
+		SET_ERR
+	}
 #endif /* __linux__ */
 
-/* System calls with changes */
-int multiplex(void) {
+	/* System calls with changes */
+	int multiplex(void) {
 #ifdef __linux__
-	return epoll_create1(0);
+		return epoll_create1(0);
 #elif defined(__APPLE__)
 	int ret = kqueue();
 	if (ret == -1) err = errno;
 	return ret;
 #endif
-}
+	}
 
-int mregister(int multiplex, int fd, int flags, void *attach) {
+	int mregister(int multiplex, int fd, int flags, void *attach) {
 #ifdef __linux__
-	struct epoll_event ev;
-	int event_flags = 0;
+		struct epoll_event ev;
+		int event_flags = 0;
 
-	if (flags & MULTIPLEX_FLAG_READ) {
-		event_flags |= EPOLLIN;
-	}
+		if (flags & MULTIPLEX_FLAG_READ) {
+			event_flags |= EPOLLIN;
+		}
 
-	if (flags & MULTIPLEX_FLAG_WRITE) {
-		event_flags |= EPOLLOUT;
-	}
+		if (flags & MULTIPLEX_FLAG_WRITE) {
+			event_flags |= EPOLLOUT;
+		}
 
-	ev.events = event_flags;
-	if (attach == NULL)
-		ev.data.fd = fd;
-	else
-		ev.data.ptr = attach;
+		ev.events = event_flags;
+		if (attach == NULL)
+			ev.data.fd = fd;
+		else
+			ev.data.ptr = attach;
 
-	if (epoll_ctl(multiplex, EPOLL_CTL_ADD, fd, &ev) < 0) {
-		if (err == EEXIST) {
-			if (epoll_ctl(multiplex, EPOLL_CTL_MOD, fd, &ev) < 0) {
+		if (epoll_ctl(multiplex, EPOLL_CTL_ADD, fd, &ev) < 0) {
+			if (err == EEXIST) {
+				if (epoll_ctl(multiplex, EPOLL_CTL_MOD, fd,
+					      &ev) < 0) {
+					return -1;
+				}
+			} else {
 				return -1;
 			}
-		} else {
-			return -1;
 		}
-	}
 
-	return 0;
+		return 0;
 #elif defined(__APPLE__)
 	struct kevent change_event[2];
 	int event_count = 0, res;
@@ -507,23 +516,23 @@ int mregister(int multiplex, int fd, int flags, void *attach) {
 	if (res < 0) return -1;
 	return 0;
 #endif
-}
-
-static int has_begun = 0;
-
-void begin(void) {
-	if (!has_begun) {
-		has_begun = 1;
 	}
-}
 
-int mwait(int multiplex, Event *events, int max_events,
-	  int64_t timeout_millis) {
+	static int has_begun = 0;
+
+	void begin(void) {
+		if (!has_begun) {
+			has_begun = 1;
+		}
+	}
+
+	int mwait(int multiplex, Event *events, int max_events,
+		  int64_t timeout_millis) {
 #ifdef __linux__
-	int timeout = (timeout_millis >= 0) ? (int)timeout_millis : -1;
+		int timeout = (timeout_millis >= 0) ? (int)timeout_millis : -1;
 
-	return epoll_wait(multiplex, (struct epoll_event *)events, max_events,
-			  timeout);
+		return epoll_wait(multiplex, (struct epoll_event *)events,
+				  max_events, timeout);
 #elif defined(__APPLE__)
 	struct timespec ts;
 	struct timespec *timeout_ptr = NULL;
@@ -540,167 +549,164 @@ int mwait(int multiplex, Event *events, int max_events,
 	if (ret == -1) err = errno;
 	return ret;
 #endif
-}
+	}
 
-int event_is_read(Event event) {
+	int event_is_read(Event event) {
 #ifdef __linux__
-	struct epoll_event *epoll_ev = (struct epoll_event *)&event;
-	return epoll_ev->events & EPOLLIN;
+		struct epoll_event *epoll_ev = (struct epoll_event *)&event;
+		return epoll_ev->events & EPOLLIN;
 #elif defined(__APPLE__)
 	struct kevent *kv = (struct kevent *)&event;
 	return kv->filter == EVFILT_READ;
 #endif
-}
+	}
 
-int event_is_write(Event event) {
+	int event_is_write(Event event) {
 #ifdef __linux__
-	struct epoll_event *epoll_ev = (struct epoll_event *)&event;
-	return epoll_ev->events & EPOLLOUT;
+		struct epoll_event *epoll_ev = (struct epoll_event *)&event;
+		return epoll_ev->events & EPOLLOUT;
 #elif defined(__APPLE__)
 	struct kevent *kv = (struct kevent *)&event;
 	return kv->filter == EVFILT_WRITE;
 #endif
-}
+	}
 
-void *event_attachment(Event event) {
+	void *event_attachment(Event event) {
 #ifdef __linux__
-	struct epoll_event *epoll_ev = (struct epoll_event *)&event;
-	return epoll_ev->data.ptr;
+		struct epoll_event *epoll_ev = (struct epoll_event *)&event;
+		return epoll_ev->data.ptr;
 #elif defined(__APPLE__)
 	struct kevent *kv = (struct kevent *)&event;
 	return kv->udata;
 #endif
-}
-
-int file(const char *path) {
-	int ret = open(path, O_CREAT | O_RDWR, 0600);
-#ifdef __APPLE__
-	if (ret == -1) err = errno;
-#endif
-	return ret;
-}
-int exists(const char *path) {
-	int fd = open(path, O_RDWR);
-	if (fd > 0) {
-		close(fd);
-		return 1;
 	}
-	return 0;
-}
 
-int64_t micros(void) {
-	struct timeval tv;
+	int file(const char *path) {
+		int ret = open(path, O_CREAT | O_RDWR, 0600);
+#ifdef __APPLE__
+		if (ret == -1) err = errno;
+#endif
+		return ret;
+	}
+	int exists(const char *path) {
+		int fd = open(path, O_RDWR);
+		if (fd > 0) {
+			close(fd);
+			return 1;
+		}
+		return 0;
+	}
 
-	if (gettimeofday(&tv, NULL) == -1) {
+	int64_t micros(void) {
+		struct timeval tv;
+
+		if (gettimeofday(&tv, NULL) == -1) {
+#ifdef __APPLE__
+			err = errno;
+#endif
+			return -1;
+		}
+
+		return (int64_t)tv.tv_sec * 1000000 + tv.tv_usec;
+	}
+
+	int sleepm(uint64_t millis) {
+		struct timespec req;
+		int ret;
+		req.tv_sec = millis / 1000;
+		req.tv_nsec = (millis % 1000) * 1000000;
+		ret = nanosleep(&req, &req);
+#ifdef __APPLE__
+		if (ret == -1) err = errno;
+#endif
+		return ret;
+	}
+
+	off_t fsize(int fd) {
+		off_t ret = lseek(fd, 0, SEEK_END);
 #ifdef __APPLE__
 		err = errno;
 #endif
-		return -1;
+		return ret;
 	}
 
-	return (int64_t)tv.tv_sec * 1000000 + tv.tv_usec;
-}
-
-int sleepm(uint64_t millis) {
-	struct timespec req;
-	int ret;
-	req.tv_sec = millis / 1000;
-	req.tv_nsec = (millis % 1000) * 1000000;
-	ret = nanosleep(&req, &req);
+	int yield(void) {
+		int ret = sched_yield();
 #ifdef __APPLE__
-	if (ret == -1) err = errno;
+		if (ret == -1) err = errno;
 #endif
-	return ret;
-}
-
-off_t fsize(int fd) {
-	off_t ret = lseek(fd, 0, SEEK_END);
-#ifdef __APPLE__
-	err = errno;
-#endif
-	return ret;
-}
-
-int yield(void) {
-	int ret = sched_yield();
-#ifdef __APPLE__
-	if (ret == -1) err = errno;
-#endif
-	return ret;
-}
-int fresize(int fd, off_t length) {
-	int ret = ftruncate(fd, length);
-#ifdef __APPLE__
-	if (ret == -1) err = errno;
-#endif
-
-	return ret;
-}
-
-void *map(size_t length) {
-	void *v = mmap(NULL, length, PROT_READ | PROT_WRITE,
-		       MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
-	if (v == MAP_FAILED) {
-#ifdef __APPLE__
-		err = errno;
-#endif
-		return NULL;
+		return ret;
 	}
-	return v;
-}
-void *fmap(int fd, off_t offset, off_t size) {
-	void *v =
-	    mmap(NULL, size, PROT_READ | PROT_WRITE, MAP_SHARED, fd, offset);
-	if (v == MAP_FAILED) {
+	int fresize(int fd, off_t length) {
+		int ret = ftruncate(fd, length);
 #ifdef __APPLE__
-		err = errno;
+		if (ret == -1) err = errno;
 #endif
 
-		return NULL;
+		return ret;
 	}
-	return v;
-}
-void *smap(size_t length) {
-	void *v = mmap(NULL, length, PROT_READ | PROT_WRITE,
-		       MAP_ANONYMOUS | MAP_SHARED, -1, 0);
-	if (v == MAP_FAILED) {
+
+	void *map(size_t length) {
+		void *v = mmap(NULL, length, PROT_READ | PROT_WRITE,
+			       MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
+		if (v == MAP_FAILED) {
 #ifdef __APPLE__
-		err = errno;
+			err = errno;
 #endif
-		return NULL;
+			return NULL;
+		}
+		return v;
 	}
-	return v;
-}
-
-pid_t two(void) {
-	pid_t pid = fork();
-	if (pid == 0) begin();
+	void *fmap(int fd, off_t offset, off_t size) {
+		void *v = mmap(NULL, size, PROT_READ | PROT_WRITE, MAP_SHARED,
+			       fd, offset);
+		if (v == MAP_FAILED) {
 #ifdef __APPLE__
-	if (pid == -1) err = errno;
+			err = errno;
 #endif
-	return pid;
-}
 
-int flush(int fd) {
-	int ret = fdatasync(fd);
+			return NULL;
+		}
+		return v;
+	}
+	void *smap(size_t length) {
+		void *v = mmap(NULL, length, PROT_READ | PROT_WRITE,
+			       MAP_ANONYMOUS | MAP_SHARED, -1, 0);
+		if (v == MAP_FAILED) {
 #ifdef __APPLE__
-	if (ret == -1) err = errno;
+			err = errno;
 #endif
-	return ret;
-}
+			return NULL;
+		}
+		return v;
+	}
 
-#include <signal.h>
-#include <sys/time.h>
-#include <time.h>
+	pid_t two(void) {
+		pid_t pid = fork();
+		if (pid == 0) begin();
+#ifdef __APPLE__
+		if (pid == -1) err = errno;
+#endif
+		return pid;
+	}
 
-typedef int timer_t;
+	int flush(int fd) {
+		int ret = fdatasync(fd);
+#ifdef __APPLE__
+		if (ret == -1) err = errno;
+#endif
+		return ret;
+	}
 
-int timer_create(clockid_t clockid, struct sigevent *sevp, timer_t *timerid);
-int timer_delete(timer_t timerid);
-int timer_settime(timer_t timerid, int flags,
-		  const struct itimerspec *new_value,
-		  struct itimerspec *old_value);
-int sigaction(int signum, const struct sigaction *act,
-	      struct sigaction *oldact);
+	/*
 
-int timeout(void (*task)(void), uint32_t milliseconds) { return 0; }
+	typedef int timer_t;
+
+	int timer_create(clockid_t clockid, struct sigevent *sevp, timer_t
+	*timerid); int timer_delete(timer_t timerid); int timer_settime(timer_t
+	timerid, int flags, const struct itimerspec *new_value, struct
+	itimerspec *old_value); int sigaction(int signum, const struct sigaction
+	*act, struct sigaction *oldact);
+
+	int timeout(void (*task)(void), uint32_t milliseconds) { return 0; }
+	*/
