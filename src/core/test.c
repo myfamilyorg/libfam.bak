30c41efbfe76c4c2f45ce1327600182cdcb3820f
Christopher Gilliard <christopher.gilliard@gmail.com>
2025-05-31 17:56:42 -0700

    alarm test

--- src/core/test.c
+++ src/core/test.c
@@ -1,10 +1,18 @@
 #include <criterion/criterion.h>
 #include <stdio.h>
 #include <sys.h>
+#include <unistd.h>
 
 void to_fun() { printf("to_fun\n"); }
 
+void handle_alarm() {
+	printf("exiting\n");
+	exit(0);
+}
+
 Test(core, timer) {
+	signal(SIGALRM, handle_alarm);
+	alarm(2);
 	timeout(to_fun, 100);
 	/*sleepm(1000);*/
 	while (true);
