f7e76cee08acb73edb0bfce7694d1cae0bc33b91
Christopher Gilliard <christopher.gilliard@gmail.com>
2025-06-01 08:07:13 -0700

    register tasks

--- src/core/timeout.c
+++ src/core/timeout.c
@@ -32,8 +32,6 @@ struct kernel_sigaction {
 	unsigned long k_sa_mask;
 };
 
-static void init_sigmask(unsigned long *mask) { *mask = 0; }
-
 static int syscall_sigaction_rt(int signum, const struct kernel_sigaction *act,
 				struct kernel_sigaction *oldact,
 				size_t sigsetsize) {
@@ -60,56 +58,51 @@ static int syscall_sigaction_rt(int signum, const struct kernel_sigaction *act,
 
 void sig_ign(int __attribute((unused)) sig) {}
 
+void (*next_task)(int);
+
+/* Signal handler for SIGALRM */
+void timeout_handler(int v) { next_task(v); }
+
 static void __attribute__((constructor)) _disable_sigpipe__(void) {
 #ifdef __linux__
 	struct kernel_sigaction act = {0};
+	struct kernel_sigaction act2 = {0};
 	act.k_sa_handler = sig_ign;
 	act.k_sa_flags = SA_RESTORER;
 	act.k_sa_restorer = sigaction_restorer;
-	init_sigmask(&act.k_sa_mask);
 	if (syscall_sigaction_rt(SIGPIPE, &act, NULL, 8) < 0) {
 		const char *msg = "WARNL could not register SIGPIPE handler\n";
 		int __attribute__((unused)) v = write(2, msg, strlen(msg));
 	}
+	act2.k_sa_handler = timeout_handler;
+	act2.k_sa_flags = SA_RESTORER;
+	act2.k_sa_restorer = sigaction_restorer;
+	syscall_sigaction_rt(SIGALRM, &act2, NULL, 8);
 #endif /* __linux__ */
 #ifdef __APPLE__
 	struct sigaction act = {0};
+	struct sigaction act2 = {0};
 	act.sa_handler = sig_ign;
 	act.sa_flags = 0;
-	sigemptyset(&act.sa_mask);
 	if (sigaction(SIGPIPE, &act, NULL) < 0) {
 		const char *msg = "WARNL could not register SIGPIPE handler\n";
 		write(2, msg, strlen(msg));
 	}
-#endif /* __APPLE__ */
+	act2.sa_handler = timeout_handler;
+	act2.sa_flags = 0;
+	sigaction(SIGALRM, &act2, NULL);
+#endif
 }
-
-/* Signal handler for SIGALRM */
-void timeout_handler(void) { printf("timeout handler\n"); }
 
 /* Timeout function */
 int timeout(void (*task)(int), uint64_t milliseconds) {
 	struct itimerval new_value;
-#ifdef __linux__
-	struct kernel_sigaction act = {0};
-	act.k_sa_handler = task;
-	act.k_sa_flags = SA_RESTORER;
-	act.k_sa_restorer = sigaction_restorer;
-	init_sigmask(&act.k_sa_mask);
-
-	syscall_sigaction_rt(SIGALRM, &act, NULL, 8);
-#endif
-#ifdef __APPLE__
-	struct sigaction act = {0};
-	act.sa_handler = task;
-	act.sa_flags = 0;
-	sigemptyset(&act.sa_mask);
-	sigaction(SIGALRM, &act, NULL);
-#endif
 	new_value.it_interval.tv_sec = 0;
 	new_value.it_interval.tv_usec = 0;
 	new_value.it_value.tv_sec = (long)(milliseconds / 1000);
 	new_value.it_value.tv_usec = (long)((milliseconds % 1000) * 1000);
+
+	next_task = task;
 
 	if (setitimer(ITIMER_REAL, &new_value, NULL) < 0) {
 		return -1;
