4ecd5cf7ade6e9ae72735945332c0cb0063d61f7
Christopher Gilliard <christopher.gilliard@gmail.com>
2025-06-06 22:35:49 +0000

    fix x64

--- src/include/atomic.h
+++ src/include/atomic.h
@@ -28,6 +28,7 @@
 
 #include <types.h>
 
+#ifdef __aarch64__
 uint32_t __cas32(uint32_t *ptr, uint32_t *expected, uint32_t desired);
 uint32_t __and32(uint32_t *ptr, uint32_t value);
 uint32_t __add32(uint32_t *ptr, uint32_t value);
@@ -39,6 +40,25 @@ uint64_t __and64(uint64_t *ptr, uint64_t value);
 uint64_t __add64(uint64_t *ptr, uint64_t value);
 uint64_t __sub64(uint64_t *ptr, uint64_t value);
 uint64_t __or64(volatile uint64_t *a, uint64_t v);
+#elif defined(__amd64__)
+
+#define __add32(a, v) __atomic_fetch_add(a, v, __ATOMIC_SEQ_CST)
+#define __sub32(a, v) __atomic_fetch_sub(a, v, __ATOMIC_SEQ_CST)
+#define __or32(a, v) __atomic_or_fetch(a, v, __ATOMIC_SEQ_CST)
+#define __and32(a, v) __atomic_and_fetch(a, v, __ATOMIC_SEQ_CST)
+#define __cas32(a, expected, desired)                            \
+	__atomic_compare_exchange_n(a, expected, desired, false, \
+				    __ATOMIC_SEQ_CST, __ATOMIC_SEQ_CST)
+
+#define __add64(a, v) __atomic_fetch_add(a, v, __ATOMIC_SEQ_CST)
+#define __sub64(a, v) __atomic_fetch_sub(a, v, __ATOMIC_SEQ_CST)
+#define __or64(a, v) __atomic_or_fetch(a, v, __ATOMIC_SEQ_CST)
+#define __and64(a, v) __atomic_and_fetch(a, v, __ATOMIC_SEQ_CST)
+#define __cas64(a, expected, desired)                            \
+	__atomic_compare_exchange_n(a, expected, desired, false, \
+				    __ATOMIC_SEQ_CST, __ATOMIC_SEQ_CST)
+
+#endif
 
 #define ALOAD(a) __atomic_load_n(a, __ATOMIC_ACQUIRE)
 #define ASTORE(a, v) __atomic_store_n(a, v, __ATOMIC_RELEASE)
