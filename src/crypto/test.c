2c6175a505cd57ce1886d18fe7b5ebec76755ee2
Christopher Gilliard <christopher.gilliard@gmail.com>
2025-06-13 19:44:03 +0000

    update to uint8_t

--- src/crypto/test.c
+++ src/crypto/test.c
@@ -30,10 +30,11 @@
 Test(aes1) {
 	const uint8_t key1[32] = {0};
 	const uint8_t iv1[16] = {0};
-	char buf[64] = {0};
+	uint8_t buf[64] = {0};
 
 	AesContext ctx;
 	aes_init(&ctx, key1, iv1);
 	aes_ctr_xcrypt_buffer(&ctx, buf, sizeof(buf));
-	/*ASSERT_EQ(buf[0], 220, "aes220");*/
+	printf("buf[0]=%i\n", buf[0]);
+	ASSERT_EQ(buf[0], 220, "aes220");
 }
