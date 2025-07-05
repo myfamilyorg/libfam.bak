b7f951913bb44329af523c1ca66c070bd534ca01
Christopher Gilliard <christopher.gilliard@gmail.com>
2025-07-04 19:18:09 -0700

    Hello world crc32c test

--- src/crypto/test.c
+++ src/crypto/test.c
@@ -141,9 +141,9 @@ Test(crc32c) {
 	/* Note: seem to produce different results than test vectors.
 	 * While non-standard it appears to work.
 	 * */
-	/*const char* hello_crc = "Hello, CRC-32C!";*/
-	/*u32 out = crc32c(hello_crc, strlen(hello_crc));*/
-	/*println("out={x}", out);*/
+	const char* hello_crc = "Hello, CRC-32C!";
+	u32 out = crc32c(hello_crc, strlen(hello_crc));
+	println("out={x}", out);
 	println("test1={x}", crc_test1(1234));
 	println("test={x}", crc32c("test", 4));
 	/*ASSERT_EQ(out, 0x6F23DDF, "Hello, CRC-32C!");*/
