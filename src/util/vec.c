0398bd49aea157e0319e3a7d3caf27014a6442c5
Christopher Gilliard <christopher.gilliard@gmail.com>
2025-07-20 21:15:28 -0700

    refactor vec

--- src/util/vec.c
+++ src/util/vec.c
@@ -30,28 +30,28 @@
 
 struct Vec {
 	u64 capacity;
-	u64 elements;
+	u64 bytes;
 };
 
 u64 vec_capacity(Vec *v) {
 	if (!v) return 0;
 	return v->capacity;
 }
-u64 vec_elements(Vec *v) {
+u64 vec_size(Vec *v) {
 	if (!v) return 0;
-	return v->elements;
+	return v->bytes;
 }
 void *vec_data(Vec *v) {
 	if (!v) return NULL;
 	return (u8 *)v + sizeof(Vec);
 }
 i32 vec_extend(Vec *v, void *data, u64 len) {
-	if (!v || len + v->elements > v->capacity) {
+	if (!v || len + v->bytes > v->capacity) {
 		err = EFAULT;
 		return -1;
 	}
-	memcpy((u8 *)v + sizeof(Vec) + v->elements, data, len);
-	v->elements += len;
+	memcpy((u8 *)v + sizeof(Vec) + v->bytes, data, len);
+	v->bytes += len;
 	return 0;
 }
 
@@ -60,18 +60,18 @@ Vec *vec_resize(Vec *v, u64 nsize) {
 	if (!ret) return NULL;
 	ret->capacity = nsize;
 	if (!v)
-		ret->elements = 0;
-	else if (ret->elements > ret->capacity)
-		ret->elements = ret->capacity;
+		ret->bytes = 0;
+	else if (ret->bytes > ret->capacity)
+		ret->bytes = ret->capacity;
 	return ret;
 }
 
-i32 vec_set_elements(Vec *v, u64 elements) {
-	if (!v || v->capacity < elements) {
+i32 vec_set_size(Vec *v, u64 bytes) {
+	if (!v || v->capacity < bytes) {
 		err = EFAULT;
 		return -1;
 	}
-	v->elements = elements;
+	v->bytes = bytes;
 	return 0;
 }
 
@@ -82,13 +82,13 @@ void vec_release(Vec *v) {
 }
 
 i32 vec_truncate(Vec *v, u64 nsize) {
-	u64 elems = vec_elements(v);
+	u64 elems = vec_size(v);
 	if (nsize > elems) {
 		err = EINVAL;
 		return -1;
 	}
 	if (elems) {
-		v->elements = nsize;
+		v->bytes = nsize;
 	}
 	return 0;
 }
